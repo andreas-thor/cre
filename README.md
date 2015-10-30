b193e6eb4b04d5ff78feeab3cc61576fe0a4194d
Andreas Thor <dr.andreas.thor@googlemail.com>
2015-10-30 22:18:57 +0100

    version of 10/30/2015 (see README.md)

--- README.md
+++ README.md
@@ -1,9 +1,36 @@
-# cre
-Cited References Explorer
+# CRE: Cited References Explorer
+
+##ToDo
+* discuss start process (java web start vs. jar vs. bat vs. exe vs. ...)
 
 
-ToDo
-+ Info fenster nicht aktualisiert
-* start process (java web start vs. jar vs. bat vs. exe vs. ...)
-+ cancel button when please wait
-* 100,000 warning statt stop
+##UPDATE 10/30/2015
+* revision of program settings (re-arranged in settings dialog, stored for next program start)
+* maximum number of CRs for import can be set (0=infinite)
+* "please wait" dialog has "cancel" button 
+
+##UPDATES 10/20/2015
+* Some classes are annotated as @CompileStatic for performance improvements
+* Generated ant file and jnlp file for web starter deployment
+* Removed matcher dialog
+* Dialog windows centered relative to main window 
+
+##UPDATES 10/15/2015
+* Window position after start: center
+* Name changed to CitedReferencesExplorer
+* Dialog to save data before opening WoS / CSV file
+* Hide clustering buttons after loading
+* Remove vertical blue line at mouse position
+* Clustering starts immediately after clicking "Cluster equivalent ..." (no dialog)
+* Renaming: "source" for "medium" and "source title" for "title" 
+* Make clustering insensitive to lower / upper case
+    
+## Clustering  
+Explanation: Each ClusterId has two components x/y. 
+If two CRs have the same clusterId (x/y) they are considered the same. 
+If two CRs share the first component but have different second components (i.e., x/y' and x/y'') they are somewhat similar but no the same. 
+In other words: The first component x reflects the result of a coarse-grained clustering.
+When sorting by clusterId, CRs with different clusterIds sharing the same first component are close together which is helpful for manual inspection.
+ 
+ 
+
--- src/cre/CitedReferencesExplorer.groovy
+++ src/cre/CitedReferencesExplorer.groovy
@@ -1,31 +1,6 @@
 package cre 
 
-/**
- * UPDATES 10/20/2015
- * Some classes are annotated as @CompileStatic for performance improvements
- * Generated ant file and jnlp file for web starter deployment
- * Removed matcher dialog
- * Dialog windows centered relative to main window 
- * 
- * UPDATES 10/15/2015
- * 
- * Window position after start: center
- * Name changed to CitedReferencesExplorer
- * Dialog to save data before opening WoS / CSV file
- * Hide clustering buttons after loading
- * Remove vertical blue line at mouse position
- * Clustering starts immediately after clicking "Cluster equivalent ..." (no dialog)
- * Renaming: "source" for "medium" and "source title" for "title" 
- * Make clustering insensitive to lower / upper case
- *  
- * Explanation: Each ClusterId has two components x/y. 
- * If two CRs have the same clusterId (x/y) they are considered the same. 
- * If two CRs share the first component but have different second components (i.e., x/y' and x/y'') they are somewhat similar but no the same. 
- * In other words: The first component x reflects the result of a coarse-grained clustering.
- * When sorting by clusterId, CRs with different clusterIds sharing the same first component are close together which is helpful for manual inspection.
- *
- *
- */
+
 
 import groovy.swing.SwingBuilder
 
@@ -60,7 +35,7 @@ JFrame mainFrame
 
 
 mainFrame = sb.frame(
-	title:"CitedReferencesExplorer (Version 2015/10/20)",  
+	title:"CitedReferencesExplorer (Version 2015/10/30)",  
 	size:[800,600],
 	windowClosing: { sb.menuExit.doClick() },
 	defaultCloseOperation:JFrame.DO_NOTHING_ON_CLOSE  // WindowConstants.EXIT_ON_CLOSE
@@ -233,7 +208,9 @@ mainFrame = sb.frame(
 				(tab.getModel() as AbstractTableModel).fireTableDataChanged()
 			})
 
-			separator()
+		}
+		
+		menu(text: "Standardization", mnemonic: 'S') {
 			
 			menuItem(text: "Cluster equivalent Cited References...", mnemonic: 'C', actionPerformed: {
 					
